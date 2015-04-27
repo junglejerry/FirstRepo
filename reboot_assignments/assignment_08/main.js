0e7462a066155a3f921f202910b7f309c886e42f
Jerry John Comellas <jerry.comellas@gmail.com>
2015-04-27 16:25:16 -0400

    Assignment 8 Complete

--- reboot_assignments/assignment_08/main.js
+++ reboot_assignments/assignment_08/main.js
@@ -1,88 +1,95 @@
-$(document).ready(function() {
+ $(document).ready(function() {
 
-    alert("click the buttons");
+     alert("click the buttons");
 
-    $("h2").hide().show("slow");
+     $("h2").hide().show("slow");
 
-    $("h3 span").click(function() {
-        alert("Check out www.rebootcoding.com");
-    });
-
-    $("button5").hover(function() {
-            $(this).css($("background", "purple"));
-        },
-        function() {
-            $(this).css("background", "purple").remove();
-        });
-
-   var name = "Jerry";
-
-    function showName(name) {}
-    showName(name);
-
-    var myArray = ["Dan", " Doug", " Jerry", " Rashad", " Ricky", " Steve", " Vanessa"];
-
-    for (i = 1; i <= 10; i++) {}
-    for (i = 0; i < myArray.length; i++) {}
-
-    var car = {
-        doors: 4,
-        honk: function() {
-        }
-    };
-    car.honk();
-
-    function Function1() {
-        document.getElementById("button1 ").innerHTML = ("Hello! My name is " + name);
-        console.log("Hello! My name is " + name);
-    };
+     $("h3 span").click(function() {
+         alert("For more information, check out www.rebootcoding.com");
+     });
 
 
-    function Function2() {
-        document.getElementById("button2 ").innerHTML = "Another one of our students is named " + (myArray[1]);
-        console.log(myArray[1]);
-    };
+     $("#button5").hover(function() {
+             var $this = $(this);
+             $this.data("color", $this.css("color")).css("background-color", "purple");
+         },
+         function() {
+             var $this = $(this);
+             $this.css("color", $this.data("color"));
+         }
+     );
 
 
-    function Function3() {
-        document.getElementById("button3 ").innerHTML = "How many students are enrolled in the Front-End Engineering Course? " + (i);
-        for (i = 1; i <= 10; i++) {
-            console.log(i);
-        }
-    };
+     $("li").each(function(index) {
+         console.log(index + ": " + $(this).text());
+     });
 
 
-    function Function4() {
-        document.getElementById("button4 ").innerHTML = "These are the names of our ReBoot students: " + (myArray);
-        console.log("Hello! My name is " + name);
-        for (i = 0; i < myArray.length; i++) {
-            console.log(myArray[i]);
-        }
-    };
+     var name = "Jerry";
+
+     function showName(name) {}
+     showName(name);
 
 
-    function Function5() {
-        var myArray = ["Dan", "Doug", "Jerry", "Rashad", "Ricky", "Steve", "Vanessa"];
-        document.getElementById("button5").addEventListener("click", function() {
-            for (j = 0; j < myArray.length; j++) {
-                if (myArray[j].substring(0, 1) === "R") {
-                    console.log(myArray[j]);
-                }
-            }
-        });
-    }
+     var myArray = [" Dan", " Doug", " Jerry", " Rashad", " Ricky", " Steve", " Vanessa"];
+
+     for (i = 1; i <= 10; i++) {}
+     for (i = 0; i < myArray.length; i++) {}
 
 
-    function Function6() {
-        document.getElementById("button6 ").innerHTML = "" + ("honk ");
-        console.log("honk ");
-        var car = {
-            doors: 4,
-            honk: function() {
-                console.log("honk ");
-            }
-        }
+     var car = {
+         doors: 4,
+         honk: function() {}
+     };
+     car.honk();
 
-    };
 
-});
+     $("#button1").click(function() {
+         document.getElementById("button1 ").innerHTML = ("Hello! My name is " + name);
+         console.log("Hello! My name is " + name);
+     });
+
+     $("#button2").click(function() {
+         document.getElementById("button2 ").innerHTML = "Another one of our students is named " + (myArray[1]);
+         console.log(myArray[1]);
+     });
+
+     $("#button3").click(function() {
+         document.getElementById("button3 ").innerHTML = "How many students are enrolled in the Front-End Engineering Course? " + (i);
+         for (i = 1; i <= 10; i++) {
+             console.log(i);
+         }
+     });
+
+     $("#button4").click(function() {
+         document.getElementById("button4 ").innerHTML = "These are the names of our ReBoot students: " + (myArray);
+         for (i = 0; i < myArray.length; i++) {
+             console.log(myArray[i]);
+         }
+     });
+
+     $("#button5").click(function() {
+         var myArray = ["Dan", "Doug", "Jerry", "Rashad", "Ricky", "Steve", "Vanessa"];
+         document.getElementById("button5").addEventListener("click", function() {
+             for (j = 0; j < myArray.length; j++) {
+                 if (myArray[j].substring(0, 1) === "S") {
+                     console.log(myArray[j]);
+                     alert(myArray[j] + " is present.");
+                 }
+             }
+         });
+     });
+
+     $("#button6").click(function() {
+         document.getElementById("button6 ").innerHTML = "" + ("Beep! Beep! ");
+         console.log("honk ");
+         var car = {
+             doors: 4,
+             honk: function() {
+                 console.log("honk ");
+             }
+         }
+     });
+
+
+ });
